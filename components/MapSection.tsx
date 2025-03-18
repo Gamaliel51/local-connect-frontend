e5d34dad78c6a2cab1da90ae697528b078c6ab68
Gamaliel51 <miraclendukwe51@gmail.com>
2025-03-18 17:11:16 +0100

    routing

--- components/MapSection.tsx
+++ components/MapSection.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Polyline, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import L from "leaflet";
+import "leaflet-routing-machine";
 
 interface MapSectionProps {
   mapCenter: [number, number];
@@ -11,13 +13,51 @@ interface MapSectionProps {
   selectedBusinessName: string;
 }
 
+// This component sets up the routing control using leaflet-routing-machine.
+function Routing({
+  userPos,
+  businessPos,
+}: {
+  userPos: [number, number];
+  businessPos: [number, number];
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    // @ts-ignore
+    const routingControl = L.Routing.control({
+      waypoints: [
+        L.latLng(userPos[0], userPos[1]),
+        L.latLng(businessPos[0], businessPos[1]),
+      ],
+      lineOptions: {
+        styles: [{ color: "blue", weight: 4 }],
+      },
+      // Disable automatic marker creation (we already have our own markers)
+      createMarker: function () {
+        return null;
+      },
+      addWaypoints: false,
+      draggableWaypoints: false,
+      fitSelectedRoutes: true,
+      showAlternatives: false,
+    }).addTo(map);
+
+    return () => {
+      map.removeControl(routingControl);
+    };
+  }, [map, userPos, businessPos]);
+
+  return null;
+}
+
 export default function MapSection({
   mapCenter,
   userPos,
   businessPos,
   selectedBusinessName,
 }: MapSectionProps) {
-  // Fix default marker icon issues
+  // Fix default marker icon issues on the client side.
   useEffect(() => {
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: "/marker-1.png",
@@ -39,7 +79,8 @@ export default function MapSection({
         <Marker position={businessPos}>
           <Popup>{selectedBusinessName}</Popup>
         </Marker>
-        <Polyline positions={[userPos, businessPos]} color="blue" />
+        {/* Add the routing control to display directions */}
+        <Routing userPos={userPos} businessPos={businessPos} />
       </MapContainer>
     </div>
   );
